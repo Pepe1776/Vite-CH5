cd323611d84e1f5a129929b51617bfd36637ee1f
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-01-06 19:11:44 -0700

    crcomlib

--- src/modules/CrComLib.ts
+++ src/modules/CrComLib.ts
@@ -1,6 +1,6 @@
 import * as CrComLib from '@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib'
 import type { UserModule } from '~/types'
 
-export const install: UserModule = () => {
-  return CrComLib
+export const install: UserModule = ({ app, router, isClient }) => {
+  return { CrComLib, app, router, isClient }
 }
